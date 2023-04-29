18710df65efb3478ce823c2628355db0b6382bf1
Luca Schifano <luca.schifano005@gmail.com>
2023-04-29 10:40:03 +0200

    Test Bounce

--- Assets/Objects/Player/Player.cs
+++ Assets/Objects/Player/Player.cs
@@ -7,13 +7,20 @@ public class Player : MonoBehaviour
 {
     private float moveInputH;
     private bool isMovingForward;
+    private bool isMovingBarward;
     private Rigidbody rb;
 
     [Header("Speed")]
+    [SerializeField] private float stepSpeed;
     [SerializeField] private float maxSpeed;
     [SerializeField] private float acceleration;
+    [SerializeField] private float acceleration2;
     [SerializeField] private float deceleration;
     private float currentSpeed;
+
+    [Header("Go Back")]
+    [SerializeField] private float backSpeed;
+    [SerializeField] private float maxBackSpeed;
 
     [Header("Rotation")]
     [SerializeField] private float rotationValue;
@@ -34,13 +41,15 @@ public class Player : MonoBehaviour
 
     private void FixedUpdate()
     {
+        float leftBrake = 1;
+        float rightBrake = 1;
+
         if (isMovingForward)
         {
-            currentSpeed += acceleration;
+            if (currentSpeed < stepSpeed) currentSpeed += acceleration;
+            else currentSpeed += acceleration2;
+            currentSpeed = Mathf.Clamp(currentSpeed, 0, maxSpeed);
 
-
-            float leftBrake = 1;
-            float rightBrake = 1;
 
             switch (brakeState)
             {
@@ -54,22 +63,32 @@ public class Player : MonoBehaviour
                     break;
             }
 
+            if (moveInputH <= -0.2f) transform.Rotate(new Vector3(0, -rotationValue * leftBrake, 0));
+            else if (moveInputH >= 0.2f) transform.Rotate(new Vector3(0, rotationValue * rightBrake, 0));
+            else transform.Rotate(new Vector3(0, 0, 0));
+
+        }
+        else if (isMovingBarward)
+        {
+            currentSpeed -= backSpeed;
+            currentSpeed = Mathf.Clamp(currentSpeed, maxBackSpeed, maxSpeed);
+
 
             if (moveInputH <= -0.2f) transform.Rotate(new Vector3(0, -rotationValue * leftBrake, 0));
             else if (moveInputH >= 0.2f) transform.Rotate(new Vector3(0, rotationValue * rightBrake, 0));
             else transform.Rotate(new Vector3(0, 0, 0));
-        
-        }else currentSpeed -= deceleration;
-
-        currentSpeed = Mathf.Clamp(currentSpeed, 0, maxSpeed);
+        }
+        else
+        {
+            currentSpeed -= deceleration;
+            currentSpeed = Mathf.Clamp(currentSpeed, 0, maxSpeed);
+        }
         rb.velocity = (transform.forward).normalized * currentSpeed;
 
+
+
     }
 
-    private void Update()
-    {
-        
-    }
 
 
     // Input system --------------------------------------------------------------------------
@@ -85,6 +104,12 @@ public class Player : MonoBehaviour
         if (context.canceled) isMovingForward = false;
     }
 
+    public void Back(InputAction.CallbackContext context)
+    {
+        if (context.started) isMovingBarward= true;
+        if (context.canceled) isMovingBarward = false;
+    }
+
     public void BrakeLeft(InputAction.CallbackContext context)
     {
         if (context.started) brakeState = Brake.Left;
@@ -96,4 +121,13 @@ public class Player : MonoBehaviour
         if (context.started) brakeState = Brake.Right;
         if (context.canceled && brakeState == Brake.Right) brakeState = Brake.Zero;
     }
+
+
+
+
+    // Collision --------------------------------------------------------------------------
+
+
+
+
 }
