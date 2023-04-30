ac2d253db2360c0aec2a3f39ff2eb843c35d0dff
Luca Schifano <luca.schifano005@gmail.com>
2023-04-30 17:49:00 +0200

    Add model + logo pizza

--- Assets/Objects/Triggers/Pizza/PizzaManager.cs
+++ Assets/Objects/Triggers/Pizza/PizzaManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PizzaManager : MonoBehaviour, ICallable
 {
@@ -9,28 +10,53 @@ public class PizzaManager : MonoBehaviour, ICallable
     [SerializeField] GameObject go;
     private bool isPizzaGet;
 
+    [Header("UI")]
+    [SerializeField] private Image logo1;
+    [SerializeField] private Image logo2;
+    [SerializeField] private GameObject logo3;
+
     public bool IsPizzaGet { get => isPizzaGet; }
 
     private void Awake()
     {
         _ref.Instance = this;
-
+        go.SetActive(true);
     }
-
     private void Update()
     {
-        if (!isPizzaGet) go.SetActive(true);
-       
+        if (!isPizzaGet)
+        {
+            logo2.color = new Color(1,1,1, Mathf.PingPong(Time.time * 2, 2));
+        }
     }
 
     public void Interacted()
     {
         isPizzaGet= true;
         go.SetActive(false);
+        ChangeVisual();
     }
 
     public void PizzaDelivered()
     {
         isPizzaGet= false;
+        go.SetActive(true);
+        ChangeVisual();
+    }
+
+    private void ChangeVisual()
+    {
+        if (isPizzaGet)
+        {
+            logo1.color = Color.gray;
+            logo2.gameObject.SetActive(false);
+            logo3.SetActive(false);
+        }
+        else
+        {
+            logo1.color = Color.white;
+            logo2.gameObject.SetActive(true);
+            logo3.SetActive(true);
+        }
     }
 }
